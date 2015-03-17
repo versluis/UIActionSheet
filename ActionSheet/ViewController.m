97aceff0d067a579229a8487840dbefaee3cb887
Jay Versluis <versluis@Mini.local>
2015-03-17 16:32:06 -0400

    initial commit

--- ActionSheet/ViewController.m
+++ ActionSheet/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIActionSheetDelegate>
 
 @end
 
@@ -16,12 +16,47 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // call that Action Sheet
+    [self showActionSheet];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Action Sheet Methods
+
+- (IBAction)triggerButton:(id)sender {
+    
+    // when that button is pressed, bring up the action sheet
+    [self showActionSheet];
+}
+
+- (void)showActionSheet {
+    
+    // bring up a UIActionSheet
+    UIActionSheet *myActionSheet;
+    myActionSheet = [[UIActionSheet alloc]initWithTitle:@"Hello there!" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:@"Cancel in Red" otherButtonTitles:@"Open in Safari", nil];
+    myActionSheet.actionSheetStyle = UIActionSheetStyleAutomatic;
+    [myActionSheet showInView:self.view];
+}
+
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    // find out which button was pressed
+    if (buttonIndex == 0) {
+        NSLog(@"This was Button 0");
+    }
+    
+    if (buttonIndex == 1) {
+        NSLog(@"This was Button 1");
+    }
+    
+    if (buttonIndex == 2) {
+        NSLog(@"This was button 2");
+    }
 }
 
 @end

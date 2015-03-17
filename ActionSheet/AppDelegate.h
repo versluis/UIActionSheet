e6376b5d61ccce6a9e78baee8f400540b6bdb768
Jay Versluis <versluis@Mini.local>
2015-03-17 16:25:11 -0400

    Initial Commit

---
+++ ActionSheet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ActionSheet
+//
+//  Created by Jay Versluis on 17/03/2015.
+//  Copyright (c) 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
